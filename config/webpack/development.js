449bc290148a0351d871f1c24d9005054c65f638
Alsha Behera <alshabehera7@gmail.com>
2025-02-28 22:51:09 +0530

    Fix the error

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,1 +1,5 @@
-404: Not Found
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const environment = require("./environment");
+
+module.exports = environment;
--- config/webpack/rules.js
+++ config/webpack/rules.js
@@ -1,1 +1,28 @@
-404: Not Found
+module.exports = [
+  {
+    test: /\.(d.ts)$/,
+    exclude: /node_modules/,
+    use: [
+      "babel-loader",
+      {
+        loader: "ignore-loader",
+      },
+    ],
+  },
+  {
+    test: /\.svg$/i,
+    use: [
+      {
+        loader: "@svgr/webpack",
+        options: {
+          svgoConfig: { plugins: ["preset-default"] },
+        },
+      },
+    ],
+  },
+  {
+    test: /\.js$/,
+    enforce: "pre",
+    use: ["source-map-loader"],
+  },
+];
