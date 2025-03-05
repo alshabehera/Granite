27b165881e69385c7de9a939b4699c9c08861381
alshabehera <alshabehera7@gmail.com>
2025-03-05 22:34:12 +0530

    Implemented create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     task = task.new(task_params)
     task.save!
-    render status: :ok, json: { notice: "Task was successfully created" }
+    render_notice("Task was successfully created")
   end
 
   private
