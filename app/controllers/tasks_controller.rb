b46278e8a5d13ba3b56fe0c2e43b5c5a21467371
alshabehera <alshabehera7@gmail.com>
2025-03-05 10:46:01 +0530

    Handled exception

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,16 @@ class TasksController < ApplicationController
     tasks = Task.all
     render status: :ok, json: { tasks: }
   end
+
+  def create
+    task = task.new(task_params)
+    task.save!
+    render status: :ok, json: { notice: "Task was successfully created" }
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
