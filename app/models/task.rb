57fefb05d61ea2dcc8bf61d78a6b58079d8738a9
alshabehera <alshabehera7@gmail.com>
2025-04-09 10:35:50 +0530

    Added optimized slug generation method

--- app/models/task.rb
+++ app/models/task.rb
@@ -33,10 +33,10 @@ class Task < ApplicationRecord
 
     def set_slug
       title_slug = title.parameterize
+      regex_pattern = "slug #{Constants::DB_REGEX_OPERATOR} ?"
       latest_task_slug = Task.where(
-        "slug LIKE ? or slug LIKE ?",
-        "#{title_slug}",
-        "#{title_slug}-%"
+        regex_pattern,
+        "^#{title_slug}$|^#{title_slug}-[0-9]+$"
       ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
       slug_count = 0
       if latest_task_slug.present?
